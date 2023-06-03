dc9f8e27cdd17bdecbfeba92b6323fab59fda0de
Lucas Silvério <silverio.des.vargas@gmail.com>
2023-06-03 10:43:59 -0300

    deploy: substituição dos arquivos de ambiente, angular

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -29,7 +29,7 @@ import { FormsModule } from '@angular/forms';
     PoTableModule,
     FormsModule,
     
-    RouterModule.forRoot([]),
+    RouterModule,
     PoTemplatesModule,
     RouterModule
   ],
